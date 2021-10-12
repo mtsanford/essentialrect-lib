4a8f0a3c506794c628ed26f3da6df52210de3dd9
Mark Sanford <marktsanford@gmail.com>
2021-10-12 11:24:35 -0700

    correct package.json & webpack.config.js dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,18 @@ module.exports = {
     clean: true,
     path: path.resolve(__dirname, 'dist'),
   },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+    },
+    'react-image-crop': {
+      commonjs: 'react-image-crop',
+      commonjs2: 'react-image-crop',
+      amd: 'react-image-crop',
+    }
+  },
   target: 'web',
 };
